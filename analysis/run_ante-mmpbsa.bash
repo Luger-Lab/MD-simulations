835dba9b8f6fe6519e0a9ccf0613ea24c014f698
shla9937 <shla9937.colorado.edu>
2020-09-21 14:14:35 -0600

    added fiji scripts

--- analysis/run_ante-mmpbsa.bash
+++
@@ -1,3 +0,0 @@
-NAME=msm0213
-END1=64
-ante-MMPBSA.py -p ../${NAME}_buffer.prmtop -c complex.prmtop -r receptor.prmtop -l ligand.prmtop -s :WAT,K+,K,Na+,NA,CL,Cl- -m :1-${END1} --radii mbondi2
--- analysis/run_mmgbsa.bash
+++
@@ -1,2 +0,0 @@
-mpirun -np 20 MMPBSA.py.MPI -i igb5.in -cp complex.prmtop -rp receptor.prmtop -lp ligand.prmtop -y stripped_and_imaged.nc
-
